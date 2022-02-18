019440a927366d81f836c01bdb991b294e59fb28
Olga Santarovich <santarovich@gmail.com>
2022-02-18 20:21:25 +0300

    phone done

--- tasks/phone.py
+++ tasks/phone.py
@@ -17,3 +17,25 @@
 Модель: {}
 Год выпуска: {}
 """
+
+
+class Phone:
+    brand: str
+    model: str
+    issue_year: int
+
+    def __init__(self, brand, model, issue_year):
+        self.brand = brand
+        self.model = model
+        self.issue_year = issue_year
+
+    def receive_call(self, name):
+        self.name = name
+        print(f"Звонит {self.name}")
+        return self
+
+    def get_info(self) -> tuple:
+        return self.brand, self.model, self.issue_year
+
+    def __str__(self):
+        return f"Бренд: {self.brand}\n\rМодель: {self.model}\n\rГод выпуска: {self.issue_year}"
